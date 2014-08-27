166cff0d5c0cdfa572f9607ba05635dd4bcb7e90
romeroplata <romeromedina@msn.com>
2014-08-27 09:38:30 -0700

    Signed-off-by: romeroplata <romeromedina@msn.com>

---
+++ class/variables.php
@@ -0,0 +1,33 @@
+<?php
+/* angel romero
+  august 27 2014
+  purpus
+
+*/
+?>
+
+
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php //declare variables
+       $hours=40;//worked 40 hours
+	   $payRate=9;//$'s/hour
+	   //calculate the paycheck
+	   $grossPay=$hours*$payRate;
+	   //output the result
+	   echo "<p>hours worked = $hours(hrs)</p>";
+	   echo '<p>pay rate =$$payrate</p>';
+	   echo "<p>pay check =$ ".$grosspay.'</p>';
+	   
+	   
+	   
+	    ?>
+</body>
+</html>
