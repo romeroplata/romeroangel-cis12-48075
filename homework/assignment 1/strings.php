54a4d34bc2d765ccb18bb68404c5dab78b5e19e2
romeroplata <romeromedina@msn.com>
2014-09-07 13:55:41 -0500

    Signed-off-by: romeroplata <romeromedina@msn.com>

---
+++ homework/assignment 1/strings.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<title>Comments</title>
+</head>
+<body>
+<?php
+
+# Script 1.6 - STRINGS.php
+# Created 09/07/2014
+# Created Angel romero
+
+
+
+//Create the variables:
+$first_name = 'Haruki';
+$last_name = 'Murakami';
+$book = 'Kafka on the Shore';
+
+//print the values
+echo "<p>The book<em>$book</em>
+was written by $first_name $last_name.<p/>\n";
+
+
+
+?>
+</body>
+</html>
