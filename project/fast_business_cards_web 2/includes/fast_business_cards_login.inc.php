f97512ec4f2d17a0117c4be109bf2ce98a924f53
romeroplata <jalpa1976>
2014-11-14 12:13:16 -0600

    Signed-off-by: romeroplata <jalpa1976>

---
+++ project/fast_business_cards_web 2/includes/fast_business_cards_login.inc.php
@@ -0,0 +1,25 @@
+<?php # pagina principal
+$page_title = 'main page';
+include ('header.html');
+?>
+
+<h1>Please Login</h1>
+<?php
+	if (isset($errors) && !empty($errors)) {
+	echo '<h1>Error!</h1>
+	<p class="error">The following error(s) occurred:<br />';
+	foreach ($errors as $msg) {
+		echo " - $msg<br />\n";
+	}
+	echo '</p><p>Please try again.</p>';
+}
+?>
+<!--display form-->
+<form action="../login.php" method="post">
+	<p>Email Address: <input type="text" name="email" size="20" maxlength="60" /> </p>
+	<p>Password: <input type="password" name="pass" size="20" maxlength="20" /></p>
+	<p><input type="submit" name="submit" value="Login" /></p>
+</form>
+<?php
+include ('footer.html');
+?>
---
+++ project/fast_business_cards_web 2/index.php
@@ -0,0 +1,14 @@
+<?php # pagina principal
+$page_title = 'main page';
+include ('includes/header.html');
+?>
+
+<h1>WELCOME</h1>
+
+	<p>Get Your Business Cards In Three Days Or Are Free</p>
+	
+	<p>Volutpat at varius sed sollicitudin et, arcu. Vivamus viverra. Nullam turpis. Vestibulum sed etiam. Lorem ipsum sit amet dolore. Nulla facilisi. Sed tortor. Aenean felis. Quisque eros. Cras lobortis commodo metus. Vestibulum vel purus. In eget odio in sapien adipiscing blandit. Quisque augue tortor, facilisis sit amet, aliquam, suscipit vitae, cursus sed, arcu lorem ipsum dolor sit amet.</p>
+
+<?php
+include ('includes/footer.html');
+?>
